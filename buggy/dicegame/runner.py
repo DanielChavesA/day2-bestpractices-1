5eba25d1e8d86531893d228f2c120a334d8a42b8
DanielChavesA <daca2@kth.se>
2025-03-04 15:29:43 +0100

    Completed

--- buggy/dicegame/runner.py
+++ buggy/dicegame/runner.py
@@ -4,18 +4,23 @@ from .utils import i_just_throw_an_exception
 class GameRunner:
 
     def __init__(self):
-        self.dice = Die.create_dice(5)
+        self.dice = Die.create_dice(5) #creates 5 dices
+        self.round = 0 # Starting round count
+        self.wins = 0   # Initialize wins count
+        self.loses = 0  # Initialize loses count
+        #import ipdb; ipdb.set_trace()
         self.reset()
 
     def reset(self):
-        self.round = 1
-        self.wins = 0
-        self.loses = 0
+        self.round += 1
+        self.dice = Die.create_dice(5) 
+        #self.wins = 0  #eliminate so it doesnt reset the count
+        #self.loses = 0
 
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value  #now it sums all the values not only the instances of object class dice
         return total
 
     @classmethod
@@ -23,9 +28,9 @@ class GameRunner:
         # Probably counts wins or something.
         # Great variable name, 10/10.
         c = 0
+        runner=cls()
         while True:
-            runner = cls()
-
+            runner.reset()
             print("Round {}\n".format(runner.round))
 
             for die in runner.dice:
@@ -35,10 +40,12 @@ class GameRunner:
             guess = int(guess)
 
             if guess == runner.answer():
+                #import ipdb; ipdb.set_trace()
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
                 c += 1
             else:
+               # import ipdb; ipdb.set_trace()
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
